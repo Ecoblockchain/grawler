9c7e8b4d01451bec028b0370559f42a393aa67fa
timakin <timaki.st@gmail.com>
2016-12-26 23:32:11 +0900

    [fix] echo routing

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func rootHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Root!")
+}
+
+func registrationHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Register!")
+}
+
+func fetchHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Fetch!")
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", rootHandler)
+	e.POST("/register", registrationHandler)
+	e.POST("/fetch/:id", fetchHandler)
+	e.Logger.Fatal(e.Start(":8080"))
+
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/register", registrationHandler)
+	http.HandleFunc("/fetch/{id}", fetchHandler)
+}
